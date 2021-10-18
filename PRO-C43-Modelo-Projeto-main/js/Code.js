85264aeb61f6cf16bb1a06ce8b9e6f29cbbc7c27
andressafonseca <86491077+andressafonseca@users.noreply.github.com>
2021-10-18 14:27:51 -0300

    Add files via upload

---
+++ PRO-C43-Modelo-Projeto-main/js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Dica: Sempre mudando, não constante!!", 100,70)
+
+    fill("white")
+    textSize(15);
+    text("C U T N I F O N", 700,150);
+    fill("lightblue");
+    text("Dica: Faz uma ação específica.", 700,170);
+
+    fill("white")
+    textSize(15);
+    text("A T E D A S B A", 100,350);
+    fill("lightblue");
+    text("Dica: Armazena dados", 100,370);
+
+}
